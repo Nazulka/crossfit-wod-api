4691025580d2ebeee86400f47963a67b49153011
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-21 18:30:12 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const v1WorkoutRouter = require("./v1/routes/workoutRoute");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+});
